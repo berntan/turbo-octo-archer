d1750db4c0e498102f9f0078f03afb3a4ac00822
Bernt Andreas Langøien <berntan@usit.uio.no>
2014-03-13 14:24:04 +0100

    added a new attribute on Chirps

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,10 +1,12 @@
 // If empty database, insert first and second chirp
 Meteor.startup(function () {
   // code to run on server at startup
+
   if (Chirps.find().count() === 0) {
     var timestamp = (new Date()).getTime();
-    Chirps.insert({text: "First chirp!", timestamp: timestamp});
-    Chirps.insert({text: "Second chirp!", timestamp: timestamp+1});
+    Chirps.insert({owner: "1", text: "First chirp!", timestamp: timestamp});
+    Chirps.insert({owner: "1", text: "Second chirp!", timestamp: timestamp+1});
+    Chirps.insert({owner: "1", text: "Third chirp!", timestamp: timestamp+2});
   }
 
 });
