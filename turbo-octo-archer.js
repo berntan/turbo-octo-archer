1a88aebb2d754dc867a94bd90c279e7f0876abc5
Bernt Andreas Langøien <berntan@usit.uio.no>
2014-03-13 11:46:54 +0100

    moved initial data to server/bootstrap.js

--- turbo-octo-archer.js
+++ turbo-octo-archer.js
@@ -21,15 +21,4 @@ if (Meteor.isClient) {
 
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-    if (Chirps.find().count() === 0) {
-      var timestamp = (new Date()).getTime();
-      Chirps.insert({text: "First chirp!", timestamp: timestamp});
-      Chirps.insert({text: "Second chirp!", timestamp: timestamp+1});
-    }
-
-  });
-}
 
