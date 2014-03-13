ba87ae02a333f292f2d92474e003acac39ee2247
Bernt Andreas Langøien <berntan@usit.uio.no>
2014-03-13 11:23:30 +0100

    redering first and second chirp

--- turbo-octo-archer.js
+++ turbo-octo-archer.js
@@ -1,6 +1,8 @@
+Chirps = new Meteor.Collection("chirps");
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to turbo.";
+    return "Welcome to turbo-octo-archer.";
   };
 
   Template.hello.events({
@@ -10,10 +12,24 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+
+
+  Template.chirp_list.chirps = function () {
+    return Chirps.find({}, {sort: {timestamp: 1}});
+  };
+
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if (Chirps.find().count() === 0) {
+      var timestamp = (new Date()).getTime();
+      Chirps.insert({text: "First chirp!", timestamp: timestamp});
+      Chirps.insert({text: "Second chirp!", timestamp: timestamp+1});
+    }
+
   });
 }
+
